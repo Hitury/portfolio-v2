861eac9b8c6180d531e6a45550448470341a10ad
Hitury <xxhituryxx@gmail.com>
2025-10-15 15:23:42 +0200

    Animations

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -17,15 +17,21 @@
 // export default Hero;
 
 import { Badge } from "@/components/ui/badge";
+import { motion } from "motion/react"
+
 
 export function Hero() {
   return (
     <section className="flex flex-col justify-start items-start w-full min-h-screen overflow-hidden px-6 sm:px-10 md:px-16 lg:px-32 py-35 md:py-45">
       {/* Availability badge */}
-      <Badge className="bg-primary/10 dark:bg-primary/20 hover:bg-primary/10 text-amber-500 shadow-none rounded-full mb-4">
+      <motion.div
+
+      >
+        <Badge className="bg-primary/10 dark:bg-primary/20 hover:bg-primary/10 text-amber-500 shadow-none rounded-full mb-4">
         <div className="h-1.5 w-1.5 rounded-full bg-primary mr-2" />
         <span className="text-primary">Available for Projects</span>
       </Badge>
+      </motion.div>
 
       {/* Headings */}
       <div className="flex flex-col justify-center items-start gap-3.5 w-full">
